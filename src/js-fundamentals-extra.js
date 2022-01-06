5394633d2e1b228b1e76e3063720ada209e6d3db
Safia <safia.ibrahim2121@gmail.com>
2022-01-06 09:46:07 +0000

    complete

--- src/js-fundamentals-extra.js
+++ src/js-fundamentals-extra.js
@@ -1,60 +1,69 @@
 // SecondsInHours
+
+// Create a function that takes a number of hours and returns the number of
+// seconds in those hours.
 //
-// Create a function that takes a number of hours and returns the number of 
-// seconds in those hours. 
-//
+
 // Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
-
+function SecondsInHours(hours) {
+  return hours * 3600;
+}
 // MilesTravelled
-//
+
 // Create a function that takes a number representing speed in miles per hour,
 // a duration in minutes, and returns the distance travelled in miles rounded
 // *up* to the nearest whole mile. You can use the Math.ceil function to
 // perform the rounding. You can assume only whole numbers will be provided
 // to the function.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
-
+function MilesTravelled(speed, minutes) {
+  const miles = (speed * minutes) / 60;
+  return Math.ceil(miles);
+}
 // KilometersToMiles
-//
-// Create a function that takes a number of kilometers and converts it to miles. 
-// For this task assume there are exactly 1.6 kilometers in a mile. The returned 
+
+// Create a function that takes a number of kilometers and converts it to miles.
+// For this task assume there are exactly 1.6 kilometers in a mile. The returned
 // mile distance should be rounded to the nearest mile. You can  use the Math.round
 // function to do this.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
+function KilometersToMiles(kilometers) {
+  const miles = kilometers / 1.6;
+  return Math.round(miles);
+}
 
-
-
-// MakeSentence
 //
-// Create a function that takes a string and returns the same string with the 
-// first letter capitalized and a full stop added on to the end. If the string 
-// already ends with a full stop, question mark (?) or an exclamation mark(!) 
+// Create a function that takes a string and returns the same string with the
+// first letter capitalized and a full stop added on to the end. If the string
+// already ends with a full stop, question mark (?) or an exclamation mark(!)
 // then a full stop should not be added.
 //
 // You may need to the use `String.substring` method.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
+function MakeSentence(string) {
+  const sentence = string[0].toUpperCase() + string.substring(1);
+  const punctuation = [".", "!", "?"];
 
-
-
+  if (punctuation.includes(sentence[string.length - 1])) {
+    return sentence;
+  }
+  return sentence + ".";
+}
 // FileExtension
 //
 // Create a function that takes a filename as a string and returns the file extension
@@ -62,83 +71,111 @@
 // string should be returned. You may need to the use `String.substring` method and
 // the `String.lastIndexOf` method.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
+function FileExtension(fileName) {
+  const extension = fileName.split(".").pop();
+  if (extension !== fileName) {
+    return extension;
+  }
+  return "";
+}
 
 // Range
 //
-// Create a function that takes an array of numbers and returns the difference 
+// Create a function that takes an array of numbers and returns the difference
 // between the highest and lowest numbers in the array. You can assume the array
 // will always contain at least 1 number (it will never be empty)
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
-
+function Range(numArrays) {
+  const highest = Math.max(...numArrays);
+  const lowest = Math.min(...numArrays);
+  numArrays = highest - lowest;
+  return numArrays;
+}
 // CheckTransactions
 //
-// Create a function that checks if a list of banking transactions is valid for 
-// a given account. 
+// Create a function that checks if a list of banking transactions is valid for
+// a given account.
 //
-// The function should accept 3 arguments. The first is an array containing a 
-// list of transactions to apply to the account balance. Positive numbers are 
-// deposits and negative numbers are withdrawals. The second argument is the 
+// The function should accept 3 arguments. The first is an array containing a
+// list of transactions to apply to the account balance. Positive numbers are
+// deposits and negative numbers are withdrawals. The second argument is the
 // account starting balance and the third is the account overdraft. The function
 // should return false if applying the list of transactions causes the account
 // balance to go below the overdraft value at any point.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
+function CheckTransactions(transactionsArr, startingBalance, accountOverdraft) {
+  for (let i of transactionsArr) {
+    startingBalance += i;
+  }
+  const finalBalance = startingBalance + accountOverdraft;
+  if (finalBalance < 0) {
+    return false;
+  } else {
+    return true;
+  }
+  // return startingBalance >= accountOverdraft;
+}
 
 // FilmsInGenre
 //
 // Create a function that takes an array of film objects, a film genre, and returns
-// an array of film names that match the supplied genre. Each film object contains a 
-// `name` key that contains the film name and a 'genre' key that contains an array 
+// an array of film names that match the supplied genre. Each film object contains a
+// `name` key that contains the film name and a 'genre' key that contains an array
 // of strings describing what genres the film belongs to. See the spec file for
 // example data.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
 
-
-
+function FilmsInGenre(filmName, filmGenre) {
+  const array = [];
+  for (let i = 0; i < filmName.length; i++) {
+    // console.log(filmGenre);
+    // console.log(filmName[0].genres.includes(filmGenre));
+    if (filmName[i].genres.includes(filmGenre)) {
+      array.push(filmName[i]["name"]);
+    }
+  }
+  return array;
+}
+// console.log(FilmsInGenre(films, "Sci-Fi"));
 // TODO: change undefined to be the name of the functions you defined
 module.exports = {
   //SecondsInHours
-  a: undefined,
+  a: SecondsInHours,
 
   //MilesTravelled,
-  b: undefined,  
+  b: MilesTravelled,
 
   //KilometersToMiles,
-  c: undefined, 
+  c: KilometersToMiles,
 
   //MakeSentence
-  d: undefined, 
+  d: MakeSentence,
 
   //FileExtension
-  e: undefined,
+  e: FileExtension,
 
   //Range
-  f: undefined,
+  f: Range,
 
   //CheckTransactions
-  g: undefined,
+  g: CheckTransactions,
 
   //FilmsInGenre
-  h: undefined,
-}
+  h: FilmsInGenre,
+};
